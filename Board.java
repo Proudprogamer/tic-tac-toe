a326c80861e301e76871abadbcc82cb40374d3ee
Siddharth PG <siddharthpg18@gmail.com>
2025-10-18 13:25:56 +0530

    added place and checkWin

--- Board.java
+++ Board.java
@@ -1,15 +1,15 @@
 import java.util.*;
 
-
-
 class Board {
 
     char[][] board;
+    int count;
 
     Board(){
         board = new char[3][3];
-        for(char[] row : board)
-            Arrays.fill(row, '#');
+        count=0;
+        for(char[] row:board)
+            Arrays.fill(row, '-');
     }
 
     void printBoard(){
@@ -26,5 +26,79 @@ class Board {
         }
     }
 
-    
+    int place(int x, int y, char symbol){
+        if(count<9)
+        {
+            if(board[x][y]=='-')
+            {
+                board[x][y] = symbol;
+                count++;
+                if(count==9)
+                    return -1;
+                return 1;
+            }
+            return -2;
+        }
+        return -1;
+    }
+
+    boolean checkWin(char symbol){
+        int consec=0;
+
+        //row
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                if(board[i][j]==symbol)
+                    consec++;
+            }
+            if(consec==3)
+                return true;
+            consec=0;
+        }
+
+        consec=0;
+        //column
+        for(int j=0;j<3;j++)
+        {
+            for(int i=0;i<3;i++)
+            {
+                if(board[i][j]==symbol)
+                    consec++;
+            }
+            if(consec==3)
+                return true;
+            consec=0;
+        }
+
+        consec=0;
+        //p-diag
+        int i=0, j=0;
+        while(i<3 && j<3)
+        {
+            if(board[i][j]==symbol)
+                consec++;
+            i++;
+            j++;
+        }
+        if(consec==3)
+            return true;
+
+
+        consec=0;
+        //diag
+        i=0;
+        j=2;
+        while(i<3 && j>=0)
+        {
+            if(board[i][j]==symbol)
+                consec++;
+            i++;
+            j--;
+        }
+        if(consec==3)
+            return true;
+        return false;
+    }
 }
--- Game.java
+++ Game.java
@@ -11,57 +11,100 @@ class Game {
     }
 
     void beginGame() {
-        // System.out.println("Welcome to TicTacToe!");
-        // System.out.println("Enter the names of the Players : ");
+        System.out.println("Welcome to TicTacToe!");
+        System.out.println("Enter the names of the Players : ");
 
-        // for(int i=0;i<2;i++)
-        // {
-        //     System.out.println("Name of Player "+(i+1)+" : ");
-        //     String name = sc.nextLine();
-        //     if(players.size()==0)
-        //     {
-        //         boolean flag = true;
-        //         while(flag)
-        //         {
-        //             System.out.println("Symbol for Player "+ (i+1) +" (X or O) : ");
-        //             char symbol = sc.next().charAt(0);
-        //             symbol = Character.toUpperCase(symbol);
+        for(int i=0;i<2;i++)
+        {
+            System.out.println("Name of Player "+(i+1)+" : ");
+            String name = sc.nextLine();
+            if(players.size()==0)
+            {
+                boolean flag = true;
+                while(flag)
+                {
+                    System.out.println("Symbol for Player "+ (i+1) +" (X or O) : ");
+                    char symbol = sc.next().charAt(0);
+                    symbol = Character.toUpperCase(symbol);
 
-        //             if(symbol=='X' || symbol=='O')
-        //             {
-        //                 Player p = new Player(name, symbol);
-        //                 players.add(p);
-        //                 flag=false;
-        //             }
-        //             else
-        //             {
-        //                 System.out.println("Please enter a valid symbol!");
-        //             }
-        //         }
-        //         sc.nextLine();
-        //     }
-        //     else
-        //     {
-        //         char s = players.get(0).symbol;
-        //         char sym='#';
-        //         if(s=='X')
-        //             sym='O';
-        //         else
-        //             sym='X';
-        //         Player p = new Player(name, sym);
-        //         players.add(p);
-        //     }
-        // }
+                    if(symbol=='X' || symbol=='O')
+                    {
+                        Player p = new Player(name, symbol);
+                        players.add(p);
+                        flag=false;
+                    }
+                    else
+                    {
+                        System.out.println("Please enter a valid symbol!");
+                    }
+                }
+                sc.nextLine();
+            }
+            else
+            {
+                char s = players.get(0).symbol;
+                char sym='#';
+                if(s=='X')
+                    sym='O';
+                else
+                    sym='X';
+                Player p = new Player(name, sym);
+                players.add(p);
+            }
+        }
         Board b = new Board();
 
         b.printBoard();
 
-        // System.out.println("List of players :");
-        // for(int i=0;i<2;i++)
-        // {
-        //     System.out.println(players.get(i).name +" "+players.get(i).symbol);
-        // }
+        boolean flag=true;
 
+        int cur_player=0;
+        System.out.println("Game starts, input format - x y");
 
+        boolean winner= false;
+        while(flag)
+        {
+            boolean turn = true;
+            while(turn)
+            {
+                System.out.println(players.get(cur_player).name+"'s Turn-("+players.get(cur_player).symbol+") : ");
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                x--;
+                y--;
+                if(x>2 || x<0 || y>2 ||y<0)
+                    System.out.println("Invalid Co-ordinates, Please enter again!");
+                else
+                {
+                    int pos = b.place(x, y, players.get(cur_player).symbol);
+                    if(pos==-2)
+                        System.out.println("Please place at an empty slot!");
+                    else
+                    {
+                        winner = b.checkWin(players.get(cur_player).symbol);
+                        turn = false;
+                        cur_player = Math.abs(cur_player-1);
+                        if(pos==-1)
+                        {
+                            turn=false;
+                            flag=false;
+                            break;
+                        }
+                    }
+                }
+            }
+            if(winner)
+                flag=false;
+            b.printBoard();
+        }
+
+        if(winner)
+        {
+            cur_player = Math.abs(cur_player-1);
+            System.out.println(players.get(cur_player).name+" won!!");
+        }
+        else
+            System.out.println("Draw!");
     }
+
 }
